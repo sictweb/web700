3628eb6a63066b0f8127861681d3186065d718fe
Patrick Crawford <patrick.crawford@senecacollege.ca>
2019-02-15 12:19:40 -0500

    updated week 1 example

--- Code Examples/week1.js
+++ Code Examples/week1.js
@@ -1,7 +1,6 @@
 // NOTES
-// to run this example, simply type "node week1.js" into the terminal
-
-// Uncomment each function to see the example:
+// to run this example, uncomment the "Code Example" function 
+// that you're interested in and type "node week1.js" into the terminal
 
 /////////////////////
 // Code Examples
@@ -157,9 +156,99 @@ function showOperators(){
     // Ternary
     var age = 12;
     console.log("(age >= 18) ? 'adult' : 'minor': " + ((age >= 18) ? 'adult' : 'minor') );
-    
 }
 
 function showExecutionFlow(){
-    console.log("TODO");
+    /**
+     * 1. Sequence example: each statement is executed one after the other
+     **/
+
+    var a = 3;
+    var b = 6;
+    var c = a + b;
+
+    console.log("c: " + c);
+
+    /**
+     * 2. Conditional examples: a decision is made based on the evaluation of an expression,
+     * and a code path (or branch) taken.
+     **/
+
+    var grade;
+    var mark = 86;
+
+    if (mark >= 90) {
+        grade = 'A+';
+    } else if (mark >= 80) {
+        grade = 'A';
+    } else if (mark >= 70) {
+        grade = 'B';
+    } else if (mark >= 60) {
+        grade = 'C';
+    } else if (mark >= 50) {
+        grade = 'D';
+    } else { 
+        grade='F';
+    }
+
+    console.log("grade: " + grade);
+
+    var gpa;
+
+    switch(grade) {
+        case 'A+':
+            // do these steps if grade is A+
+            gpa = 4.0;
+            break;
+        case 'A':
+            // do these steps if grade is A
+            gpa = 4.0;
+            break;
+        case 'B':
+            // do these steps if grade is B
+            gpa = 3.0;
+            break;
+        case 'C':
+            // do these steps if grade is C
+            gpa = 2.0;
+            break;
+        case 'D':
+            // do these steps if grade is D
+            gpa = 1.0;
+            break;
+        default:
+            // do these steps in any other case
+            gpa = 0;
+            break;
+    }
+
+    console.log("gpa: " + gpa);
+
+    /**
+     * 3. Looping example: a set of statements are repeated
+     **/
+
+    var total = 0;
+    for(var i = 1; i < 11; i++) {
+        total += i;
+        console.log("i", i, "total", total);
+    }
+
+
+    /**
+     * 4. Transfer example: a set of statements are repeated
+     **/
+
+    function add(a, b) {        // declaring the add function
+        if(!b) {                // check if the b argument exists/has a value
+            return a;           // if not, simply return the value of argument a
+        }
+        return a + b;           // otherwise, return the two arguments added together
+    }
+
+    var addTotal;
+    addTotal = add(56);            // invoking the add function with a single argument
+    addTotal = add(addTotal, 92);     // invoking the add function with two arguments
+
+    console.log("addTotal: " + addTotal);
 }
